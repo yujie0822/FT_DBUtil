bb707a62833ebd966f5107f4cad567538f560d3f
Jimmy Yu <jimmyyu@fovisio.com>
2017-05-03 16:53:05 +0800

    init

---
+++ UdcView.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import cx_Oracle
+import sys
+stdout = sys.stdout
+stdin = sys.stdin
+stderr = sys.stderr
+reload( sys )
+sys.setdefaultencoding('utf-8')
+sys.stdout = stdout
+sys.stdin = stdin
+sys.stderr = stderr
+
+class Jde(object):
+    def __init__(self):
+        try:
+            self._conn = cx_Oracle.connect('jdetest/jdetest@192.168.0.238:1521/E1DB')
+            self._cursor = self._conn.cursor()
+            print "Connected"
+        except Exception,e:
+            print 'Connect failed! Error{}.{}'.format(e.args[0],e.args[1])
+            sys.exit()
+
+    def getPlineView(self):
+        """
+        @summary：查询产品线视图
+        @return：result list [[UDC1,Pline1],[UDC2,Pline2],......]
+        """
+        self._cursor.execute ("select * from edi.v_wf_udc_pline")
+        row = self._cursor.fetchall()
+        return row
+
+
+
+    def dispose(self):
+        self._cursor.close()
+        self._conn.close()
+        print "Disconnected to Database"
+
+    def __del__(self):
+        self._cursor.close()
+        self._conn.close()
+        print "Disconnected to Database"
