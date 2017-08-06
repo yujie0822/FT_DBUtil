9ccda50563c0f02fc59734ecd1349ef521fa724a
Jimmy Yu（俞杰） <jimmyyu@fovisio.com>
2017-08-06 23:01:50 +0800

    change log dir

--- Report/ReportLog.py
+++ Report/ReportLog.py
@@ -1,7 +1,7 @@
 import logging
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
-handler = logging.FileHandler('Log_Report.log')
+handler = logging.FileHandler('D:/pyLog/Log_Report.log')
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
